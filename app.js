97de9fd7deca40902d976ccc931c8690729c523c
Zé Moreira <emaildemoreira@gmail.com>
2017-12-05 17:41:33 -0300

    app listen
    app listen atualizado

--- app.js
+++ app.js
@@ -22,5 +22,6 @@ app.get(`*`, function(req, res) {
 });
 
 
-app.listen(3000);
-console.log("porta aberta");
+http.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', http.address().port);
+});
